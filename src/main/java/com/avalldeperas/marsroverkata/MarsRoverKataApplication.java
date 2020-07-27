aa55b8461cf8b86af69325939071719c69718b62
Albert Valldeperas <albertvalldeperas@gmail.com>
2020-07-27 13:16:23 +0200

    upload project

---
+++ src/main/java/com/avalldeperas/marsroverkata/MarsRoverKataApplication.java
@@ -0,0 +1,13 @@
+package com.avalldeperas.marsroverkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsRoverKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avalldeperas/marsroverkata/MarsRoverKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalldeperas.marsroverkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
